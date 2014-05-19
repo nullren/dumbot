98d765fde8538c01433f209a833655fd5e1eb77a
James Baumgarten <jebaum@ucla.edu>
2014-05-19 13:33:58 -0700

    add all event handlers and skeleton logic functions

--- dumbot.py
+++ dumbot.py
@@ -11,12 +11,6 @@ class DumBot(irc.IRCClient):
         return self.factory.nickname
     nickname = property(_get_nickname)
 
-    def signedOn(self):
-        logic.signedOn(self)
-
-    def joined(self, channel):
-        logic.joined(self, channel)
-
     def privmsg(self, user, channel, msg):
         print('PRIVMSG: ' + user + '::' + channel + '::' + msg)
         if (msg.startswith(self.nickname + ': reload') or (self.nickname == channel and msg == 'reload')):
@@ -30,6 +24,53 @@ class DumBot(irc.IRCClient):
                 return
         logic.privmsg(self, user, channel, msg)
 
+    def created(self, when):
+        logic.created(self, when)
+    def yourHost(self, info):
+        logic.yourHost(self, info)
+    def myInfo(self, servername, version, umodes, cmodes):
+        logic.myInfo(self, servername, version, umodes, cmodes)
+    def luserClient(self, info):
+        logic.luserClient(self, info)
+    def bounce(self, info):
+        logic.bounce(self, info)
+    def isupport(self, options):
+        logic.isupport(self, options)
+    def luserChannels(self, channels):
+        logic.luserChannels(self, channels)
+    def luserOp(self, ops):
+        logic.luserOp(self, ops)
+    def luserMe(self, info):
+        logic.luserMe(self, info)
+    def joined(self, channel):
+        logic.joined(self, channel)
+    def left(self, channel):
+        logic.left(self, channel)
     def noticed(self, user, channel, msg):
         logic.noticed(self, user, channel, msg)
-
+    def modeChanged(self, user, channel, set, modes, args):
+        logic.modeChanged(self, user, channel, set, modes, args)
+    def pong(self, user, secs):
+        logic.pong(self, user, secs)
+    def signedOn(self):
+        logic.signedOn(self)
+    def kickedFrom(self, channel, kicker, message):
+        logic.kickedFrom(self, channel, kicker, message)
+    def nickChanged(self, nick):
+        logic.nickChanged(self, nick)
+    def userJoined(self, user, channel):
+        logic.userJoined(self, user, channel)
+    def userLeft(self, user, channel):
+        logic.userLeft(self, user, channel)
+    def userQuit(self, user, quitMessage):
+        logic.userQuit(self, user, quitMessage)
+    def userKicked(self, kickee, channel, kicker, message):
+        logic.userKicked(self, kickee, channel, kicker, message)
+    def action(self, user, channel, data):
+        logic.action(self, user, channel, data)
+    def topicUpdated(self, user, channel, newTopic):
+        logic.topicUpdated(self, user, channel, newTopic)
+    def userRename(self, oldname, newname):
+        logic.userRename(self, oldname, newname)
+    def receivedMOTD(self, motd):
+        logic.receivedMOTD(self, motd)
--- logic.py
+++ logic.py
@@ -1,21 +1,95 @@
 #!/usr/bin/python2
 
-def signedOn(dumbot):
-    dumbot.join(dumbot.factory.channel)
-    print("Signed on as %s." % (dumbot.nickname))
-
-def joined(dumbot, channel):
-    print("Joined %s." % (channel))
-
 def privmsg(dumbot, user, channel, msg):
     if channel == dumbot.nickname:
         print('I was queried!')
+        if getUsername(user) == dumbot.owner:
+            if msg == 'join stapler':
+                dumbot.join('stapler')
+
     if channel:
         if dumbot.nickname in msg:
             dumbot.say(channel, 'suck me')
 
+def created(dumbot, when):
+    print('created')
+
+def yourHost(dumbot, info):
+    print('yourHost')
+
+def myInfo(dumbot, servername, version, umodes, cmodes):
+    print('myInfo')
+
+def luserClient(dumbot, info):
+    print('luserClient')
+
+def bounce(dumbot, info):
+    print('bounce')
+
+def isupport(self, options):
+    print('isupport')
+
+def luserChannels(dumbot, channels):
+    print('luserChannels')
+
+def luserOp(dumbot, ops):
+    print('luserOp')
+
+def luserMe(self, info):
+    print('luserMe')
+
+def joined(dumbot, channel):
+    print("Joined %s." % (channel))
+
+def left(dumbot, channel):
+    print('left')
+
 def noticed(dumbot, user, channel, msg):
     print('NOTICED ' + channel + ' | ' + user + ' ' + msg)
+
+def modeChanged(dumbot, user, channel, set, modes, args):
+    print('modeChanged')
+
+def pong(self, user, secs):
+    print('pong')
+
+def signedOn(dumbot):
+    dumbot.join(dumbot.factory.channel)
+    print("Signed on as %s." % (dumbot.nickname))
+
+def kickedFrom(dumbot, channel, kicker, message):
+    print('kickedFrom')
+
+def nickChanged(dumbot, nick):
+    print('nickChanged')
+
+def userJoined(dumbot, user, channel):
+    print('userJoined')
+
+def userLeft(dumbot, user, channel):
+    print('userLeft')
+
+def userQuit(dumbot, user, quitMessage):
+    print('userQuit')
+
+def userKicked(dumbot, kickee, channel, kicker, message):
+    print('userKicked')
+
+def action(dumbot, user, channel, data):
+    username = getUsername(user)
+    print('action')
+    # dumbot.msg(username, 'why the fuck would you "' + data + '" you faggot')
+    dumbot.say(channel, 'why the fuck would you "' + data + '" you faggot')
+
+def topicUpdated(dumbot, user, channel, newTopic):
+    print('topicUpdated')
+
+def userRename(dumbot, oldname, newname):
+    print('userRename')
+
+def receivedMOTD(dumbot, motd):
+    print('receivedMOTD')
+
 
 #### utility functions
 def getUsername(user):
