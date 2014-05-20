e9085b3e8145ba1a09e3ab58cc58f12f97198dbc
James Baumgarten <jebaum@ucla.edu>
2014-05-19 22:03:43 -0700

    semi clean implementation of commands

--- logic.py
+++ logic.py
@@ -3,47 +3,32 @@
 import re
 
 def privmsg(dumbot, user, channel, msg):
-    ### message in query
-    if channel == dumbot.nickname:
-        # TODO more thorough owner checking than just nick - maybe hostmask?
-        # TODO lots of repeated code in here, there's probably a better way to handle it
-        if getUsername(user) in dumbot.owners:
-            if msg.startswith('join ') and msg.count(' ') == 1:
-                newchannel = msg.lstrip('join ')
-                if validName(newchannel):
-                    dumbot.join(newchannel)
-                    print('joined #' + newchannel)
+    # TODO more thorough owner checking than just nick - maybe hostmask?
+    username = getUsername(user)
+    if username in dumbot.owners:
+        if channel == dumbot.nickname:               ### query
+            replylocation = username
+            command       = msg
+        elif msg.startswith(dumbot.nickname + ': '): ### hilight in channel
+            replylocation = channel
+            command       = msg[len(dumbot.nickname + ': '):]
+        else:
+            return
 
-            if msg.startswith('part ') and msg.count(' ') == 1:
-                oldchannel = msg.lstrip('part ')
-                if validName(oldchannel):
-                    dumbot.leave(oldchannel)
-                    print('left #' + oldchannel)
+        if command.startswith('join ') and command.count(' ') == 1:
+            joinChannel(dumbot, command[len('join '):])
 
-            if msg.startswith('add owner ') and msg.count(' ') == 2:
-                newowner = msg.lstrip('add owner ')
-                if validName(newowner) and newowner not in dumbot.owners:
-                    dumbot.owners.append(newowner)
-                    print('added owner ' + newowner)
-                    print('current list: %s' % dumbot.owners)
-                    with open(dumbot.factory.ownersfile, 'w') as f:
-                        for owner in dumbot.owners:
-                            f.write(owner + '\n')
+        elif command.startswith('part ') and command.count(' ') == 1:
+            partChannel(dumbot, command[len('part '):])
 
-            if msg.startswith('remove owner ') and msg.count(' ') == 2:
-                oldowner = msg.lstrip('remove owner ')
-                if validName(oldowner) and oldowner in dumbot.owners and oldowner != dumbot.owners[0]:
-                    dumbot.owners.remove(oldowner)
-                    print('removed owner ' + oldowner)
-                    print('current list: %s' % dumbot.owners)
-                    with open(dumbot.factory.ownersfile, 'w') as f:
-                        for owner in dumbot.owners:
-                            f.write(owner + '\n')
+        elif command == "list owners":
+            listOwners(dumbot, replylocation)
 
-    ### message in channel
-    if channel:
-        if dumbot.nickname in msg:
-            dumbot.say(channel, 'LolLeRbEArS')
+        elif command.startswith('add owner ') and command.count(' ') == 2:
+            addOwner(dumbot, command[len('add owner '):], replylocation)
+
+        elif command.startswith('remove owner ') and command.count(' ') == 2:
+            removeOwner(dumbot, command[len('remove owner '):], username, replylocation)
 
 def created(dumbot, when):
     print('created')
@@ -112,8 +97,7 @@ def userKicked(dumbot, kickee, channel, kicker, message):
 def action(dumbot, user, channel, data):
     username = getUsername(user)
     print('action')
-    # dumbot.msg(username, 'why the fuck would you "' + data + '" you faggot')
-    dumbot.say(channel, 'why the fuck would you "' + data + '" you faggot')
+    dumbot.say(channel, 'what kind of idiot would ' + data + ', what a stupid thing to do')
 
 def topicUpdated(dumbot, user, channel, newTopic):
     print('topicUpdated')
@@ -131,3 +115,42 @@ def getUsername(user):
 
 def validName(name, search=re.compile(r'[^-_`~|@#%^&*+a-z0-9]').search):
     return not bool(search(name))
+
+def joinChannel(dumbot, channel):
+    if validName(channel):
+        dumbot.join(channel)
+        print('joined #' + channel)
+
+def partChannel(dumbot, channel):
+    if validName(channel):
+        dumbot.leave(channel)
+        print('left #' + channel)
+
+def addOwner(dumbot, owner, replylocation):
+    if validName(owner) and owner not in dumbot.owners:
+        dumbot.owners.append(owner)
+        print('added owner ' + owner)
+        print('current list: %s' % dumbot.owners)
+        with open(dumbot.factory.ownersfile, 'w') as f:
+            for owner in dumbot.owners:
+                f.write(owner + '\n')
+
+def removeOwner(dumbot, owner, remover, replylocation):
+    if (validName(owner)) and (owner in dumbot.owners):
+        if owner == dumbot.owners[0]:
+            dumbot.msg(replylocation, 'you mutinous bitch')
+            owner = getUsername(remover)
+            if owner == dumbot.owners[0] or owner not in dumbot.owners:
+                return
+        dumbot.owners.remove(owner)
+        dumbot.msg(replylocation, 'removed owner ' + owner)
+        with open(dumbot.factory.ownersfile, 'w') as f:
+            for owner in dumbot.owners:
+                f.write(owner + '\n')
+
+def listOwners(dumbot, replylocation):
+    ownerstring = ""
+    for owner in dumbot.owners:
+        ownerstring += owner + ', '
+    ownerstring = ownerstring[:-2]
+    dumbot.msg(replylocation, ownerstring)
